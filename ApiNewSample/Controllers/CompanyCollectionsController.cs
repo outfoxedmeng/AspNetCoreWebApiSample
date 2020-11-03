f974846cca69fd0eadc0d1b6d60a7eab000a4087
comPC <outfoxed@163.com>
2020-11-03 17:31:14 +0800

    添加一组资源

---
+++ ApiNewSample/Controllers/CompanyCollectionsController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiNewSample.Dtos;
+using ApiNewSample.Entities;
+using ApiNewSample.Helper;
+using ApiNewSample.Services;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace ApiNewSample.Controllers
+{
+    [Route("api/companycollections")]
+    [ApiController]
+    public class CompanyCollectionsController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly ICompanyRepository _repository;
+
+        public CompanyCollectionsController(IMapper mapper, ICompanyRepository repository)
+        {
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+
+        [HttpGet("({ids})", Name = nameof(GetCompanyCollections))]
+        public async Task<IActionResult> GetCompanyCollections([FromRoute][ModelBinder(binderType: typeof(ArrayModelBinder))] IEnumerable<int> ids)
+        {
+            //注意：null值判断
+            if (ids == null)
+            {
+                return BadRequest();
+            }
+
+            var companies = await _repository.GetCompaniesAsync(ids);
+
+            //注意：ids的长度要与数据个数对应，否则返回404
+            if (ids.Count() != companies.Count())
+            {
+                return NotFound();
+            }
+
+            var companyDtos = _mapper.Map<IEnumerable<CompanyDto>>(companies);
+            return Ok(companyDtos);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCompanies(IEnumerable<CompanyAddDto> addDtos)
+        {
+            var companies = _mapper.Map<IEnumerable<Company>>(addDtos);
+
+            foreach (var c in companies)
+            {
+                _repository.AddCompany(c);
+            }
+            await _repository.SaveChangesAsync();
+
+
+            var dtosToReturn = _mapper.Map<IEnumerable<CompanyDto>>(companies);
+
+            var idsString = string.Join(',', dtosToReturn.Select(x => x.Id));
+
+            return CreatedAtRoute(nameof(GetCompanyCollections), new { ids = idsString }, dtosToReturn);
+        }
+
+
+    }
+}
---
+++ ApiNewSample/Helper/ArrayModelBinder.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace ApiNewSample.Helper
+{
+    public class ArrayModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext == null)
+            {
+                throw new ArgumentNullException(nameof(bindingContext));
+            }
+            //判断Model是不是Enumerable类型（是不是作用在可枚举类型上）
+            if (!bindingContext.ModelMetadata.IsEnumerableType)
+            {
+                bindingContext.Result = ModelBindingResult.Failed();
+                return Task.CompletedTask;
+            }
+
+            var value = bindingContext.ValueProvider.GetValue(bindingContext.ModelName).ToString();
+
+            //注意：空值需要先设置Result,再返回
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                bindingContext.Result = ModelBindingResult.Success(null);
+                return Task.CompletedTask;
+            }
+
+            var values = value.Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+            var valueType = bindingContext.ModelMetadata.ModelType.GenericTypeArguments[0];
+            //var valueType2 = bindingContext.ModelType;
+
+            //var eq = object.ReferenceEquals(valueType, valueType2);//true
+            //var eq2 = valueType == valueType2;//true
+
+            var typeConverter = TypeDescriptor.GetConverter(valueType);
+
+            var valueObjs = values.Select(v => typeConverter.ConvertFromString(v.Trim())).ToArray();
+
+            var typedValues = Array.CreateInstance(valueType, values.Length);
+            //将一个 Array 的一部分元素复制到另一个 Array 中，并根据需要执行类型转换和装箱。
+            Array.Copy(valueObjs, typedValues, values.Length);
+
+
+            //设置Model
+            bindingContext.Model = typedValues;
+
+            bindingContext.Result = ModelBindingResult.Success(bindingContext.Model);
+            return Task.CompletedTask;
+        }
+    }
+}
