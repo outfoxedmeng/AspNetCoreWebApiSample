7a8bbd0a2039d1646c5343591351fbcc99b3bb6b
comPC <outfoxed@163.com>
2020-11-04 11:53:11 +0800

    输入验证 DataAnnotations

--- ApiNewSample/Dtos/CompanyAddDto.cs
+++ ApiNewSample/Dtos/CompanyAddDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,16 @@ namespace ApiNewSample.Dtos
     //P21
     public class CompanyAddDto
     {
+        [Display(Name = "名")]
+        [Required(ErrorMessage = "{0}是必须的")]
+        [MaxLength(100, ErrorMessage = "{0}的最大长度为{1}")]
         public string Name { get; set; }
         public string Country { get; set; }
         public string Industry { get; set; }
         public string Product { get; set; }
+
+        [Display(Name = "jianjie")]
+        [StringLength(500, MinimumLength = 10, ErrorMessage = "{0}的长度范围是从{2}到{1}")]
         public string Introduction { get; set; }
 
         public DateTime? BankruptTime { get; set; }
